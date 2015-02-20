b1968340113e55a3a779ee87d7c3e1effe30bf13
bhavin0810 <bhavin0810@gmail.com>
2015-02-20 12:03:08 -0500

    add the button object to optimize the code

--- COMP2068_Assignment2_SlotMachine/scripts/_references.js
+++ COMP2068_Assignment2_SlotMachine/scripts/_references.js
@@ -1,4 +1,11 @@
-﻿/// <autosync enabled="true" />
+﻿//Porject Name: Assignment2-Slot Machine
+//Programmer: Bhavin Patel
+//Description: refernces for the all API
+//Date: Feb 20 2015
+//version 1.0 Add references for the All APIs.
+
+
+/// <autosync enabled="true" />
 /// <reference path="lib/easeljs-0.8.0.min.js" />
 /// <reference path="lib/soundjs-0.6.0.min.js" />
 /// <reference path="lib/preloadjs-0.6.0.min.js" />
